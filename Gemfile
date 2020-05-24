e2f704cd6cb409bbffab0b70c0414b82d210a3b3
Pat Allan <pat@freelancing-gods.com>
2020-05-24 21:45:42 +1000

    Add Bugsnag

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem "sass-rails", ">= 6"
 gem "turbolinks", "~> 5"
 gem "webpacker", "~> 4.0"
 
+gem "bugsnag"
 gem "bootsnap", ">= 1.4.2", :require => false
 gem "decent_exposure"
 gem "devise"
---
+++ config/initializers/bugsnag.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+Bugsnag.configure do |config|
+  config.api_key               = ENV["BUGSNAG_API_KEY"]
+  config.notify_release_stages = %w[ production staging ]
+  config.release_stage         = ENV["BUGSNAG_RELEASE_STAGE"]
+end
