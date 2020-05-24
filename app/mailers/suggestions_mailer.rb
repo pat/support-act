fcaeade67a087306a56787f8b76af5750b94283c
Pat Allan <pat@freelancing-gods.com>
2020-05-24 16:57:16 +1000

    Use shared methods for latest albums/purchases.

--- app/mailers/suggestions_mailer.rb
+++ app/mailers/suggestions_mailer.rb
@@ -3,13 +3,8 @@
 class SuggestionsMailer < ApplicationMailer
   def suggest(fan)
     @fan = fan
-
-    @albums = Album.
-      where(:id => fan.provider_cache["latest_album_ids"][0..19]).
-      includes(:artist).
-      order(:id)
-
-    @purchases = Purchase.where(:fan => fan, :album => @albums)
+    @albums = Album.latest_for_fan(fan)
+    @purchases = Purchase.for_fan_and_albums(fan, @albums)
 
     mail :to => fan.email, :subject => "Monthly Suggestions"
   end
