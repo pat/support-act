c001c5be938d6a0142ccac9e5a4a42a5e628c399
John Barton <jb@johnbarton.co>
2020-07-27 20:44:19 +1000

    Don't authenticate spotify in dev if you don't have creds.

--- config/initializers/spotify.rb
+++ config/initializers/spotify.rb
@@ -1,3 +1,10 @@
 # frozen_string_literal: true
 
-RSpotify.authenticate(ENV["SPOTIFY_CLIENT_ID"], ENV["SPOTIFY_CLIENT_SECRET"])
+spotify_client_id = ENV["SPOTIFY_CLIENT_ID"]
+spotify_client_secret = ENV["SPOTIFY_CLIENT_SECRET"]
+
+if spotify_client_id && spotify_client_secret
+  RSpotify.authenticate(spotify_client_id, spotify_client_secret)
+elsif Rails.env.production?
+  raise "No spotify creds provided"
+end
