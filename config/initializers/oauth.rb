693696bef3f233fbcd69a2830e0d80fd8cc92719
Pat Allan <pat@freelancing-gods.com>
2021-04-25 19:14:05 +1000

    chore: update rspotify to include OAuth fix.

--- config/initializers/oauth.rb
+++ config/initializers/oauth.rb
@@ -6,16 +6,3 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :spotify, ENV["SPOTIFY_CLIENT_ID"], ENV["SPOTIFY_CLIENT_SECRET"],
     :scope => "user-top-read"
 end
-
-# Fix for: https://github.com/guilhermesad/rspotify/issues/189
-# This can be removed if/when https://github.com/guilhermesad/rspotify/pull/196
-# is merged in.
-module OmniAuth
-  module Strategies
-    class Spotify < OmniAuth::Strategies::OAuth2
-      def callback_url
-        full_host + script_name + callback_path
-      end
-    end
-  end
-end
