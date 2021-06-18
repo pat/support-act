41e6c0d72889c01b968a1e3e13c3acae21801411
Pat Allan <pat@freelancing-gods.com>
2021-06-18 21:02:53 +1000

    fix: return something useful for invalid mime-types.

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -1,5 +1,20 @@
 # frozen_string_literal: true
+
 # Be sure to restart your server when you modify this file.
 
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
+
+class InvalidMimeTypeMiddleware
+  def initialize(app)
+    @app = app
+  end
+
+  def call(env)
+    @app.call(env)
+  rescue ActionDispatch::Http::MimeNegotiation::InvalidType => error
+    [406, {"Content-Type" => "text/plain"}, [error.message]]
+  end
+end
+
+Rails.application.config.middleware.use InvalidMimeTypeMiddleware
