6fe13037407b1fbb6fa0902cadb208156e3bcda3
Pat Allan <pat@freelancing-gods.com>
2021-08-08 17:02:12 +1000

    chore: Upgrade webpacker to 6.0.0 beta.7
    Thankfully hadn't done really anything with the defaults, so following
    the recommended upgrade path was easy enough.

--- app/packs/entrypoints/application.js
+++ app/packs/entrypoints/application.js
@@ -1,12 +1,11 @@
+/* eslint no-console:0 */
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
+// a relevant structure within app/packs and only use these pack files to reference
 // that code so it'll be compiled.
-
-require("@rails/ujs").start();
-require("turbolinks").start();
-require("@rails/activestorage").start();
-require("channels");
+//
+// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
+// layout file, like app/views/layouts/application.html.erb
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -14,6 +13,14 @@ require("channels");
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
+
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
 
 window.setAsPurchased = function (id) {
   var album = document.getElementById(`album-${id}`);
